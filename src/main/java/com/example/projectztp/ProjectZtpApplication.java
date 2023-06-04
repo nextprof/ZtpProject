960555d1aebe237728bdc49579e9ecf0506c92a8
nextprof <kubabednarowicz8@wp.pl>
2023-06-04 18:32:06 +0200

    init

---
+++ src/main/java/com/example/projectztp/ProjectZtpApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectztp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectZtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectZtpApplication.class, args);
+	}
+
+}
